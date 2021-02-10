f85078210099fa230d2ffe7486e99330a8022828
Alessandro <alessandro.boscato@gmail.com>
2021-02-10 17:09:20 +0100

    commit

---
+++ database/seeders/ServersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Server;
+
+class ServersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
